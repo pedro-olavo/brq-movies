635c529b3e2f0c95acb93ffebed4f8562c1f3930
pedro-olavo <polavo00@gmail.com>
2024-06-20 17:18:04 -0300

    feat: adds SplashScreen

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,23 @@
 import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen'
 
 import Routes from './routes';
 import { AppProvider } from './contexts/app';
+import { useEffect } from 'react';
+
 
 export function App() {
+
+  useEffect(() => {
+    SplashScreen.show()
+
+    const timer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <AppProvider>
       <Routes />
